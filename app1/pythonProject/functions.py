9e0aea9c88a1e50d0dbf338d34a1e10986770617
amit-malik-lab49 <90905418+amit-malik-lab49@users.noreply.github.com>
2023-12-21 13:55:49 +0530

    initiation
    initial commit

---
+++ app1/pythonProject/functions.py
@@ -0,0 +1,14 @@
+file_path = 'todos.txt'
+
+
+def get_todos(filepath=file_path):
+    """ Read the text file and return the to-do items."""
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=file_path):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
