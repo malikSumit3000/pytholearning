edc13ff560df4c8d31cba430c7efc90fde58fc21
malikSumit3000 <116936402+malikSumit3000@users.noreply.github.com>
2024-01-06 08:34:38 +0530

    app4 - Add rows to the table and add headers

--- app4-invoice-generation/main.py
+++ app4-invoice-generation/main.py
@@ -16,9 +16,29 @@ for filepath in filepaths:
     pdf.add_page()
     pdf.set_font(family="Times", size=16, style="B")
 
+    # Add invoice number
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", align="L", ln=1)
 
+    # Add date
     pdf.cell(w=50, h=8, txt=f"Date: {date}", align="L", ln=1)
 
-    pdf.output(f"PDFs/{filename}.pdf")
+    # Add Headers
+    columns = list(df.columns)
+    columns = [items.replace('_', ' ').title() for items in columns]
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=60, h=8, txt=columns[1], border=1)
+    pdf.cell(w=32, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
 
+    # Add rows to the table
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
+        pdf.cell(w=60, h=8, txt=str(row['product_name']), border=1)
+        pdf.cell(w=32, h=8, txt=str(row['amount_purchased']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
+        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+
+    pdf.output(f"PDFs/{filename}.pdf")
