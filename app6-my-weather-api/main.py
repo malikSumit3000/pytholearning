4dc85892427210ff691b77d2a9bc770f1a250ce9
malikSumit3000 <116936402+malikSumit3000@users.noreply.github.com>
2025-03-10 09:12:16 +0530

    app6- integrated Weather API

--- app6-my-weather-api/main.py
+++ app6-my-weather-api/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
+
     return render_template("home.html")
 
 
@@ -15,13 +17,14 @@ def about():
 
 @app.route('/api/v1/<station>/<date>')
 def temp(station, date):
-    station = 10
-    date = 20030225
-    temperature = 0.2
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    df["TG"] = df['   TG'].loc[df['   TG'] != -9999] / 10
+    temperature = df["TG"].loc[df['    DATE'] == date].squeeze()
     return {"station": station,
             "date": date,
             "temperature": temperature}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
